86f866909fe8b406682bd0a86b896191818966aa
Keviner123 <kevin@kevinfrost.dk>
2021-08-09 13:10:57 +0200

    v2

--- Webbrowser/Webbrowser/Program.cs
+++ Webbrowser/Webbrowser/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,36 +11,50 @@ namespace Webbrowser
 {
     class Program
     {
+
         public static string StripHTML(string input)
         {
             //Remove all HTML tags, by using a regex.
             return Regex.Replace(input, "<.*?>", String.Empty);
         }
+
+        private static async Task<string> GetWebsiteContent(string url)
+        {
+            var uri = new Uri(url);
+            var client = new HttpClient();
+            var response = await client.GetStringAsync(uri);
+            return(response);
+        }
+
         static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
         {
             while (true)
             {
                 WebClient client = new WebClient();
-
-                Console.WriteLine("Enter Website to browse: ");
+                Console.Write("Enter Website to browse: ");
                 string RequestURL = Console.ReadLine();
 
                 try
                 {
-                    string WebsiteContent = client.DownloadString(RequestURL);
-                    Console.WriteLine(StripHTML(WebsiteContent));
+                    var WebsiteContent = await GetWebsiteContent(RequestURL);
 
-                    Console.WriteLine("Press return to make a new request:");
+                    //Strip the HTML tags
+                    WebsiteContent = StripHTML(WebsiteContent);
+
+                    Console.WriteLine(WebsiteContent);
+                    Console.WriteLine("Press any button to make a new request: ");
                     Console.ReadKey();
                 }
                 catch (Exception)
                 {
-
                     Console.WriteLine("Invalid URL");
                 }
             }
-           
-
         }
     }
 }
